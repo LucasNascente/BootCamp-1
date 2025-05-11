245f63dcead828b947c1e741af3227ae21081626
Lucas Nascente <147668620+LucasNascente@users.noreply.github.com>
2025-05-11 17:06:43 -0300

    Delete projeto py directory

--- projeto py/teste.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def dobro_raiz(a):
-  a = float(input("Digite número: "))
-  dobro = a * 2
-  raiz = math.sqrt(a)
-  print("O dobro do número é:", dobro)
-  print("A raiz quadrada do número é:", raiz)
