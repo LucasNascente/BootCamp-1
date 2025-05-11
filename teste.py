22679cae413290657eb88aeb349991ff1b208e5f
Lucas Nascente <147668620+LucasNascente@users.noreply.github.com>
2025-05-11 17:08:33 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,8 @@
+import math
+
+def dobro_raiz(a):
+  a = float(input("Digite número: "))
+  dobro = a * 2
+  raiz = math.sqrt(a)
+  print("O dobro do número é:", dobro)
+  print("A raiz quadrada do número é:", raiz)
